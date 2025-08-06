c42c6edf9108ba15fa58c6d7db9f23ef6db4d117
pheldu95 <phelps.duncan@gmail.com>
2025-08-06 16:07:56 -0500

    added useState and useEffect to App.tsx. Reviewing basic React stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [campsites, setCampsites] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/campsites')
+    .then(response => response.json)
+    .then(data => setCampsites(data));
+  }, [])
 
   return (
-    <h3>Route Mapper</h3>
+    <div>
+      <h3>Route Mapper</h3>
+      <ul>
+        {campsites.map((campsite) => (
+          <li key={campsite.id}>{campsite.description}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
