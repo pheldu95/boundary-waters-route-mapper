c69cca8a2146fba6ba51a4c7299b1ab9fdf4d1b7
pheldu95 <phelps.duncan@gmail.com>
2025-09-04 16:24:43 -0500

    created handleSubmit function in CampsiteForm. Next will actually get the function to create/edit a campsite.

--- client/src/features/campsites/form/CampsiteForm.tsx
+++ client/src/features/campsites/form/CampsiteForm.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import type { FormEvent } from "react";
 
 type Props ={
   campsite?: Campsite
@@ -6,20 +7,35 @@ type Props ={
 }
 
 export default function CampsiteForm({campsite, closeForm}: Props) {
+  
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); //prevents browser reload on submit
+    
+    const formData =  new FormData(event.currentTarget);
+
+    //the name of each key come from the "name" property on each textfield
+    const data: {[key: string]: FormDataEntryValue} = {}
+    formData.forEach((value, key) => {
+      data[key] = value;
+    })
+
+    console.log(data);
+  }
+  
   return (
     <Paper sx={{borderRadius: 3, padding: 3}}>
         <Typography variant="h5" gutterBottom color="primary">
             Add Campsite
         </Typography>
-        <Box component='form' display='flex' flexDirection='column' gap={3}>
-            <TextField label='Name' value={campsite?.name}/>
-            <TextField label='Latitude'/>
-            <TextField label='Longitude'/>
-            <TextField label='Description' multiline rows={3}/>
-            <TextField label='Url'/>
+        <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
+            <TextField name='name' label='Name' value={campsite?.name}/>
+            <TextField name='latitude' label='Latitude' value={campsite?.latitude}/>
+            <TextField name='longitude' label='Longitude' value={campsite?.longitude}/>
+            <TextField name='description' label='Description' value={campsite?.description} multiline rows={3}/>
+            <TextField name='url' label='Url' value={campsite?.url}/>
             <Box display='flex' justifyContent='end' gap={3}>
                 <Button onClick={closeForm} color='inherit'>Cancel</Button>
-                <Button color='success' variant="contained">Submit</Button>
+                <Button type="submit" color='success' variant="contained">Submit</Button>
             </Box>
         </Box>
     </Paper>
