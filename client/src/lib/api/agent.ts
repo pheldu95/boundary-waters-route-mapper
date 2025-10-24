08769e1138966f16c5ed56dfb5f9bac20d2a27c0
pheldu95 <phelps.duncan@gmail.com>
2025-10-24 16:58:38 -0500

    created agent.ts to handle creating an axios instance. Also adds a fake delay to API to simulate loading

---
+++ client/src/lib/api/agent.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+//creating a sleep function to add fake delay to our app
+const sleep = (delay: number) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay)
+    })
+}
+const agent = axios.create({
+    baseURL: import.meta.env.VITE_API_URL //from .env.development
+})
+
+agent.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch(error) {
+        console.log(error);
+        return Promise.reject(error);     
+    }
+})
+
+export default agent;
--- client/src/lib/hooks/useCampsites.ts
+++ client/src/lib/hooks/useCampsites.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import agent from "../api/agent";
 
 //custom hook to get all campsites
 export const useCampsites = () => {
     const { data: campsites, isPending } = useQuery({
         queryKey: ['campsites'],
         queryFn: async () => {
-            const response = await axios.get<Campsite[]>('https://localhost:5001/api/campsites');
+            const response = await agent.get<Campsite[]>('/campsites');
             return response.data;
         }
     })
