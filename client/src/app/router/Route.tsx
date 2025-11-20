2925b7f385cb7fffaad994a8559c46444c0c9591
pheldu95 <phelps.duncan@gmail.com>
2025-11-20 12:40:17 -0600

    added react-router to app. Created route for home page

---
+++ client/src/app/router/Route.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import App from "../layout/App";
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+])
